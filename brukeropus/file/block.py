c0d6154da2680a109c90641160ef526740e4e3e6
Josh Duran <josh.m.duran@gmail.com>
2024-05-26 21:44:46 -0400

    update imports and documention for block.py

--- brukeropus/file/block.py
+++ brukeropus/file/block.py
@@ -1,5 +1,3 @@
-# import datetime, warnings
-# import numpy as np
 from brukeropus.file.utils import get_block_type_label, get_data_key
 from brukeropus.file.parser import (parse_header,
                                     parse_directory,
@@ -17,7 +15,7 @@ class FileBlock:
     '''Generic OPUS file block.
 
     This class initializes with the most basic file block info from the file directory: type, size, and start location
-    as well as the raw bytes from the file (to assign block bytes).
+    as well as the raw bytes from the file (which can subsequently be parsed).
 
     Args:
         filebytes: raw bytes of the file
@@ -30,7 +28,7 @@ class FileBlock:
         size: size of block in number of bytes
         start: pointer to start location of the block within the file
         bytes: raw bytes of file block (set to zero bytes if successfully parsed)
-        data: parsed data if successful. Could be: list, str, np.ndarray or dict depending on the block type.
+        data: parsed data if successful. Could be: `list`, `str`, `np.ndarray` or `dict` depending on the block type.
         parser: name of parsing function if parsing was successful
     '''
 
@@ -72,11 +70,11 @@ class FileBlock:
         return self.type[2] > 1 and self.type[1] == 2
 
     def is_param(self):
-        '''Returns True if `FileBlock` is a parameter block (could be data status, rf param, sample param, etc.)'''
+        '''Returns True if `FileBlock` is any parameter block (could be data status, rf param, sample param, etc.)'''
         return self.type[2] > 0 or self.type == (0, 0, 0, 0, 0, 1)
 
     def is_sm_param(self):
-        '''Returns True if `FileBlock` is a prameter block associated with sample/result measurement (not including
+        '''Returns True if `FileBlock` is a parameter block associated with sample/result measurement (not including
         data status blocks)'''
         return self.is_param() and not self.is_data_status() and not self.is_rf_param()
 
@@ -101,10 +99,10 @@ class FileBlock:
         return get_block_type_label(self.type)
 
     def get_data_key(self):
-        '''If block is a data block, this function will return an shorthand key to reference that data.
+        '''If block is a data block, this function will return a shorthand key to reference that data.
 
-        e.g. t: transmission, a: absorption, sm: sample, rf: reference, smph: sample phase etc. If the block is not
-        a data block, it will return None.'''
+        e.g. t: transmission, a: absorption, sm: sample, rf: reference, phsm: sample phase etc. If the block is not
+        a data block, it will return `None`.'''
         if self.is_data() or self.is_data_series():
             return get_data_key(self.type)
         else:
@@ -126,6 +124,8 @@ class FileBlock:
             return None
 
     def parse(self):
+        '''Determines the appropriate parser for the block and parses the raw bytes.  Parsed data is stored in `data`
+        attribute and `bytes` attribute is set empty to save memory'''
         parser = self.get_parser()
         if parser is not None:
             self._try_parser(parser)
@@ -136,9 +136,8 @@ class FileDirectory:
 
     `FileDirectory` information is decoded from the raw file bytes of an OPUS file. First the header is read which
     provides the start location of the directory block, number of blocks in file, and maximum number of blocks the file
-    supports. Then it decodes the block pointer information from each entry of the file's directory block.  This class
-    intially holds all the parsed blocks from the file in `self.blocks`.  As these blocks are assigned to `OPUSFile`
-    attributes, they are removed from `self.blocks` to save memory.
+    supports. Then it decodes the block pointer information from each entry of the file's directory block to create a
+    `FileBlock` instance, initiates the block parsing, and adds the parsed block to the `blocks` attribute.
 
     Args:
         filebytes: raw bytes from OPUS file. see: `brukeropus.file.parser.read_opus_file_bytes`
@@ -147,15 +146,7 @@ class FileDirectory:
         start: pointer to start location of the directory block
         max_blocks: maximum number of blocks supported by file
         num_blocks: total number of blocks in the file
-        data_blocks: list of `FileBlockInfo` that contain array data (e.g. sample, reference, phase)
-        data_status_blocks: list of `FileBlockInfo` that contain metadata specific to a data block (units, etc.)
-        param_blocks: list of `FileBlockInfo` that contain metadata about the measurement sample
-        rf_param_blocks: list of `FileBlockInfo` that contain metatdata about the reference measurement
-        directory_block: `FileBlockInfo` for directory block that contains all the block info in the file
-        file_log_block: `FileBlockInfo` of the file log (changes, etc.)
-        data_and_status_block_pairs: (data: `FileBlockInfo`, data_status: `FileBlockInfo`) which pairs the data status
-            parameter block (time, x units, y units, etc.) with the data block it informs
-        unknown_blocks: list of `FileBlockInfo` with an unrecognized type (i.e. not sure how to parse)
+        blocks: list of `FileBlock` from the file. The class parses these blocks upon initilization of the class.
     '''
     def __init__(self, filebytes: bytes):
         self.version, self.start, self.max_blocks, self.num_blocks = parse_header(filebytes)
@@ -171,18 +162,19 @@ def is_data_status_type_match(data_block: FileBlock, data_status_block: FileBloc
     '''Checks if data and data status blocks are a match based soley on the block type.
 
     This check correctly and accurately matches blocks most of the time, but is occasionally not sufficient on its own
-    (e.g. when multiple spectra of the same type are stored in the file)'''
+    (e.g. when multiple spectra of the same exact type are stored in a single file)'''
     t1 = data_status_block.type
     t2 = data_block.type
     return t1[:2] == t2[:2] and t1[3:] == t2[3:]
 
 
 def is_data_status_val_match(data_block: FileBlock, data_status_block: FileBlock) -> bool:
-    '''Checks if min(data) and max(data) match up with the data status: MNY and MXY.
+    '''Checks if min(data) and max(data) match up with the data status parameters: MNY and MXY.
 
     When multiple spectra of the same type exist in a file, this is used to distinguish if the data and data status
     blocks are a good match.  This can reduce the number of duplicate matches, but is not generally sufficient to
-    fully eliminate duplicate matches. 
+    fully eliminate duplicate matches.
+    
     See test file: `Test Vit C_Glass.0000_comp.0`'''
     if data_block.is_data():
         try:
@@ -195,13 +187,14 @@ def is_data_status_val_match(data_block: FileBlock, data_status_block: FileBlock
         except:
             return True # If error, can't rule out the match
     else:
-        return True # Don't rule out data series
+        return True # Don't rule out data series at this time (no example files to test)
 
 
 def is_valid_match(data_block: FileBlock, data_status_block: FileBlock) -> bool:
-    '''Checks that number of points in data status are less than or equal to length of parsed data.
+    '''Checks that number of points in data status are less than or equal to length of parsed data block.
 
     This does not apply to data series. While rare, it is occasionally necessary to remove these bad matches.
+    
     See test file: `unreadable.0000`'''
     if data_block.is_data() and len(data_block.data) < data_status_block.data['npt']:
         return False
@@ -210,22 +203,25 @@ def is_valid_match(data_block: FileBlock, data_status_block: FileBlock) -> bool:
 
 
 def pair_data_and_status_blocks(blocks: list) -> list:
-    '''Takes a list of `FileBlock` and returns a list of matching (Data, Data Status) blocks for further processing.
+    '''Takes a list of `FileBlock` and returns a list of matching (data, data status) blocks for further processing.
 
     All valid data blocks have an associated data status parameter block that contains y-scaling and x-axis info.
     Generally, these blocks can be easily paired with one another by using the block type. However, some files can
     contain multiple data blocks with the same exact type, which leads to duplicate matches and requires further
     inspection to accurately pair.  This function uses the following logical sequence to accurately pair these blocks:
 
-        1. Pair by type and isolate singular matches
+        1. Pair by type and isolate singular matches from duplicate matches
         2. For duplicate matches, check min(data) and max(data) match data status MNY and MXY (including CSF scaling)
-        3. For matches that are still duplicate, remove any matches that are already in the singular match list.
-        4. Remove and matches from the singular match list where len(data) is < data status NPT (invalid data block)
-        5. Sorts the pairs in reverse order of where the data blocks are stored in the file (presume last is most
+        3. Again isolate singular matches from duplicate matches
+        4. For remaining duplicate matches, remove any matches that are already in the singular match list.
+        5. Again isolate singular matches (ideally no remaining duplicate matches at this point)
+        6. Remove invalid matches from the singular match list if len(data) is < data status NPT (invalid condition)
+        7. Sort the matches in reverse order of where the data blocks are stored in the file (presume last is most
            recently added, and therefore final spectra which was true for limited test files).
 
     For the very limited test files available, this is sufficient to accurately pair all the blocks, but it seems
-    plausible that it will not be sufficient for all files that have duplicate data type entries.  
+    plausible that it will not be sufficient for all files that have duplicate data type entries. More test files
+    required for thorough testing.
     '''
     data_status = [b for b in blocks if b.is_data_status() and type(b.data) is not str]
     data = [b for b in blocks if b.is_data() or b.is_data_series() and type(b.data) is not str]
