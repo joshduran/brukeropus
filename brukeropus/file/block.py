2abc2a0faddec8ce67ccf571aaccb1b6e39d83d0
Vikram KVNG <32128870+kvngvikram@users.noreply.github.com>
2025-10-17 15:32:45 +0530

    Add 'error' to __slots__ in block class
    When Report() function fails to read block, it throws a Exception.
    This exception is caught by try-expect block  and stored as an attribute 'error'. For this, the block should accept the 'error' attribute.

--- brukeropus/file/block.py
+++ brukeropus/file/block.py
@@ -106,7 +106,7 @@ class FileBlock:
         parser: name of parsing function if parsing was successful
     '''
 
-    __slots__ = ('type', 'size', 'start', 'bytes', 'data', 'parser', 'parse_error', 'keys', 'debug')
+    __slots__ = ('type', 'size', 'start', 'bytes', 'data', 'parser', 'parse_error', 'keys', 'debug', 'error')
 
     def __init__(self, filebytes: bytes, block_type: tuple, size: int, start: int, debug=False):
         self.bytes = filebytes[start: start + size]
